5c13b7af5fac15f32e8741591929cfbd4fffa919
Kieran Klukas <me@dunkirk.sh>
2026-01-05 20:25:17 -0500

    chore: delete other stuff

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -18,9 +18,6 @@ import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.libs.FieldConstants;
-import frc.robot.subsystems.ClimberSubsystem;
-import frc.robot.subsystems.ElevatorSubsystem;
-import frc.robot.subsystems.ScoringSubsystem;
 import frc.robot.subsystems.TargetingSubsystem;
 import frc.robot.subsystems.TargetingSubsystem.Side;
 import frc.robot.subsystems.swervedrive.SwerveSubsystem;
@@ -56,9 +53,6 @@ public class RobotContainer {
 	private final SwerveSubsystem drivebase = new SwerveSubsystem(
 		new File(Filesystem.getDeployDirectory(), "swerve/neo")
 	);
-	private final ElevatorSubsystem elevatorSubsystem = new ElevatorSubsystem();
-	private final ScoringSubsystem scoringSubsystem = new ScoringSubsystem();
-	private final ClimberSubsystem climberSubsystem = new ClimberSubsystem();
 	private final TargetingSubsystem targetingSubsystem = new TargetingSubsystem();
 
 	/** ----------
@@ -122,15 +116,9 @@ public class RobotContainer {
 	 * ---
 	 */
 	public RobotContainer() {
-		NamedCommands.registerCommand("score", score);
-		NamedCommands.registerCommand("intake", intake);
 		NamedCommands.registerCommand("driveToLeftBranch", driveToLeftBranch);
 		NamedCommands.registerCommand("driveToRightBranch", driveToRightBranch);
 		NamedCommands.registerCommand("driveToSource", driveToSource);
-		NamedCommands.registerCommand("L1", L1);
-		NamedCommands.registerCommand("L2", L2);
-		NamedCommands.registerCommand("L3", L3);
-		NamedCommands.registerCommand("L4", L4);
 		configureBindings();
 		DriverStation.silenceJoystickConnectionWarning(true);
 
@@ -245,251 +233,8 @@ public class RobotContainer {
 				)
 			);
 
-		driverXbox.rightBumper().and(DriverStation::isTest).onTrue(scoringSubsystem.runIntakeCommand());
-
 		driverXbox.rightTrigger().and(DriverStation::isTeleop).onTrue(climberSubsystem.playMusicCommand());
-
-		/** -------------------------------------
-		 * Scoring Bindings
-		 * ---
-		 * positions are staggered so it is in an increasing u shape on the joystick
-		 * demo command will be toggled via binding and will continuially run elevator positions
-		 * manual control sets elevator position and speed based on joystick position
-		 * ---
-		 */
-
-		m_JoystickL
-			.trigger()
-			.onTrue(
-				Commands.either(
-					scoringSubsystem.runIntakeCommand(),
-					scoringSubsystem.runEjectCommand(),
-					scoringSubsystem::getCoralSensor
-				)
-			);
-
-		m_JoystickL.trigger().and(m_JoystickL.button(7)).whileTrue(scoringSubsystem.runEjectCommand());
-		m_JoystickL.trigger().and(m_JoystickL.button(8)).whileTrue(scoringSubsystem.runIntakeCommand());
-
-		/** -------------------------------------
-		 * Elevator position bindings
-		 * ---
-		 * positions are staggered so it is in an increasing u shape on the joystick
-		 * demo command will be toggled via binding and will continuially run elevator positions
-		 * manual control sets elevator position and speed based on joystick position
-		 * ---
-		 */
-
-		m_JoystickL
-			.trigger()
-			.and(DriverStation::isTest)
-			.whileTrue(elevatorSubsystem.setSpeed(() -> m_JoystickL.getY() * -1));
-
-		m_JoystickL
-			.button(2)
-			.and(() -> !DriverStation.isTest())
-			.whileTrue(elevatorSubsystem.setSpeed(() -> m_JoystickL.getY() * -1));
-
-		m_JoystickL.button(5).onTrue(L1);
-		m_JoystickL.button(3).onTrue(L2);
-		m_JoystickL.button(4).onTrue(L3);
-		m_JoystickL.button(6).onTrue(L4);
-
-		m_JoystickL
-			.button(7)
-			.toggleOnTrue(
-				Commands.either(
-					Commands.repeatingSequence(
-						elevatorSubsystem
-							.setPos(() -> FieldConstants.CoralStation.height)
-							.alongWith(scoringSubsystem.tiltCommand(FieldConstants.CoralStation.pitch)),
-						Commands.waitSeconds(2),
-						elevatorSubsystem
-							.setPos(() -> FieldConstants.ReefHeight.L2.height)
-							.alongWith(scoringSubsystem.tiltCommand(FieldConstants.ReefHeight.L2.pitch)),
-						Commands.waitSeconds(2),
-						elevatorSubsystem
-							.setPos(() -> FieldConstants.ReefHeight.L3.height)
-							.alongWith(scoringSubsystem.tiltCommand(FieldConstants.ReefHeight.L3.pitch)),
-						Commands.waitSeconds(2),
-						elevatorSubsystem
-							.setPos(() -> FieldConstants.ReefHeight.L4.height)
-							.alongWith(scoringSubsystem.tiltCommand(FieldConstants.ReefHeight.L4.pitch))
-							.andThen(scoringSubsystem.tiltCommand(144)),
-						Commands.waitSeconds(2)
-					),
-					Commands.none(),
-					DriverStation::isTest
-				)
-			);
-
-		/** -------------------------------------
-		 * Climber bindings
-		 * ---
-		 * climb and descend
-		 * ---
-		 */
-
-		m_JoystickL.button(9).whileTrue(climberSubsystem.descendCommand().alongWith(Commands.print("climber down")));
-		m_JoystickL.button(10).whileTrue(climberSubsystem.climbCommand().alongWith(Commands.print("climber up")));
-
-		/** -------------------------------------
-		 * Test-mode specific tilt bindings
-		 * ---
-		 * tilt command (button 8) and tilt nudge
-		 * tilt sysid command
-		 * ---
-		 */
-
-		m_JoystickL.button(8).and(DriverStation::isTest).whileTrue(scoringSubsystem.tiltCommand(0));
-
-		Command tiltJoyCommand = scoringSubsystem.tiltJoy(() -> m_JoystickL.getY());
-		tiltJoyCommand.addRequirements(scoringSubsystem);
-		scoringSubsystem.setDefaultCommand(tiltJoyCommand);
-
-		m_JoystickL.button(12).onTrue(scoringSubsystem.zeroEncoder());
-
-		/** -------------------------------------
-		 * Test-mode specific sysid bindings
-		 * ---
-		 * sysid command for elevator, tilt, and drivebase
-		 * ---
-		 */
-
-		m_JoystickL
-			.button(2)
-			.onTrue(
-				Commands.either(scoringSubsystem.tiltSysIDCommand(4, 2, 2), Commands.none(), DriverStation::isTest)
-			);
-
-		m_JoystickR
-			.button(9)
-			.whileTrue(
-				Commands.either(elevatorSubsystem.sysIDCommand(4, 2, 2), Commands.none(), DriverStation::isTest)
-			);
-
-		m_JoystickR
-			.button(10)
-			.whileTrue(
-				Commands.either(scoringSubsystem.tiltSysIDCommand(4, 2, 2), Commands.none(), DriverStation::isTest)
-			);
-		m_JoystickR
-			.button(11)
-			.whileTrue(Commands.either(drivebase.sysIdAngleMotorCommand(), Commands.none(), DriverStation::isTest));
-		m_JoystickR
-			.button(12)
-			.whileTrue(Commands.either(drivebase.sysIdDriveMotorCommand(), Commands.none(), DriverStation::isTest));
 	}
-
-	/** ----------
-	 * Auto named commands
-	 * ---
-	 * named commands for reef align and auto scoring on different levels
-	 * ---
-	 */
-	public Command score = Commands.waitUntil(
-		() -> scoringSubsystem.atDesiredPosition() && elevatorSubsystem.atDesiredPosition()
-	)
-		.withTimeout(1.5)
-		.andThen(scoringSubsystem.runEjectCommand());
-	public Command intake = scoringSubsystem
-		.tiltCommand(FieldConstants.CoralStation.pitch)
-		.andThen(
-			Commands.waitUntil(() -> scoringSubsystem.atDesiredPosition() & elevatorSubsystem.atDesiredPosition())
-				.withTimeout(1.5)
-				.andThen(scoringSubsystem.runIntakeCommand())
-				.andThen(
-					new PrintCommand("Coral sensor active, running intake").until(() ->
-						!scoringSubsystem.getCoralSensor()
-					)
-				)
-		);
-	public Command driveToLeftBranch = targetingSubsystem.driveToLeftBranch(drivebase);
-	public Command driveToRightBranch = targetingSubsystem.driveToRightBranch(drivebase);
-	public Command driveToSource = targetingSubsystem.driveToSourceCommand(drivebase);
-	public Command L1 = elevatorSubsystem
-		.setPos(() -> FieldConstants.CoralStation.height)
-		.alongWith(scoringSubsystem.tiltCommand(FieldConstants.CoralStation.pitch))
-		.until(elevatorSubsystem::atDesiredPosition);
-	public Command L2 = elevatorSubsystem
-		.setPos(() -> FieldConstants.ReefHeight.L2.height)
-		.alongWith(scoringSubsystem.tiltCommand(FieldConstants.CoralStation.pitch))
-		.until(elevatorSubsystem::atDesiredPosition)
-		.andThen(scoringSubsystem.tiltCommand(FieldConstants.ReefHeight.L2.pitch))
-		.until(scoringSubsystem::atDesiredPosition);
-	public Command L3 = elevatorSubsystem
-		.setPos(() -> FieldConstants.ReefHeight.L3.height)
-		.alongWith(scoringSubsystem.tiltCommand(FieldConstants.CoralStation.pitch))
-		.until(elevatorSubsystem::atDesiredPosition)
-		.andThen(scoringSubsystem.tiltCommand(FieldConstants.ReefHeight.L3.pitch))
-		.until(scoringSubsystem::atDesiredPosition);
-	public Command L4 = elevatorSubsystem
-		.setPos(() -> FieldConstants.ReefHeight.L4.height)
-		.alongWith(scoringSubsystem.tiltCommand(FieldConstants.CoralStation.pitch))
-		.until(elevatorSubsystem::atDesiredPosition)
-		.andThen(scoringSubsystem.tiltCommand(FieldConstants.ReefHeight.L4.pitch))
-		.until(scoringSubsystem::atDesiredPosition);
-
-	public Command autoTargetLeftBranchCommand = targetingSubsystem
-		.autoTargetPairCommand(drivebase::getPose, Side.LEFT)
-		.andThen(
-			Commands.either(
-				targetingSubsystem.driveToCoralTarget(drivebase),
-				Commands.runEnd(
-					() -> driverXbox.getHID().setRumble(RumbleType.kBothRumble, 1),
-					() -> driverXbox.getHID().setRumble(RumbleType.kBothRumble, 0)
-				).withTimeout(.2),
-				() -> targetingSubsystem.areWeAllowedToDrive(drivebase::getPose)
-			)
-		)
-		.withName("autoTargetLeftBranch");
-
-	public Command autoTargetRightBranchCommand = targetingSubsystem
-		.autoTargetPairCommand(drivebase::getPose, Side.RIGHT)
-		.andThen(
-			Commands.either(
-				targetingSubsystem.driveToCoralTarget(drivebase),
-				Commands.runEnd(
-					() -> driverXbox.getHID().setRumble(RumbleType.kBothRumble, 1),
-					() -> driverXbox.getHID().setRumble(RumbleType.kBothRumble, 0)
-				).withTimeout(.2),
-				() -> targetingSubsystem.areWeAllowedToDrive(drivebase::getPose)
-			)
-		)
-		.withName("autoTargetRightBranch");
-
-	public Command autoLvl4Command = elevatorSubsystem
-		.setPos(() -> FieldConstants.ReefHeight.L4.height)
-		.alongWith(scoringSubsystem.tiltCommand(FieldConstants.ReefHeight.L4.pitch))
-		.until(elevatorSubsystem::atDesiredPosition)
-		.andThen(scoringSubsystem.runIntakeCommand())
-		.withName("autoLvl4");
-
-	public Command autoLvl3Command = elevatorSubsystem
-		.setPos(() -> FieldConstants.ReefHeight.L3.height)
-		.alongWith(scoringSubsystem.tiltCommand(FieldConstants.ReefHeight.L3.pitch))
-		.until(elevatorSubsystem::atDesiredPosition)
-		.andThen(scoringSubsystem.runIntakeCommand())
-		.withName("autoLvl3");
-
-	public Command autoLvl2Command = elevatorSubsystem
-		.setPos(() -> FieldConstants.ReefHeight.L2.height)
-		.alongWith(scoringSubsystem.tiltCommand(FieldConstants.ReefHeight.L2.pitch))
-		.until(elevatorSubsystem::atDesiredPosition)
-		.andThen(scoringSubsystem.runEjectCommand())
-		.withName("autoLvl2");
-
-	public Command autoIntakeCommand = elevatorSubsystem
-		.setPos(() -> FieldConstants.CoralStation.height)
-		.alongWith(scoringSubsystem.tiltCommand(FieldConstants.CoralStation.pitch))
-		.until(elevatorSubsystem::atDesiredPosition)
-		.andThen(scoringSubsystem.runIntakeCommand())
-		.withName("autoIntake");
-
-	public Command autoAlignSourceCommand = targetingSubsystem
-		.driveToSourceCommand(drivebase)
-		.alongWith(autoIntakeCommand)
-		.withName("autoAlignSource");
 
 	/** ----------
 	 * Use this to pass the autonomous command to the main {@link Robot} class.
