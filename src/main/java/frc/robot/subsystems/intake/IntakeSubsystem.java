f8b512676f37ed19b2e526884016cc94dd68b57b
Gnomecromancer <139186467+Gnomecromancer@users.noreply.github.com>
2026-03-10 08:47:05 -0400

    feat: add intake homing command

--- src/main/java/frc/robot/subsystems/intake/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeSubsystem.java
@@ -116,6 +116,9 @@ public class IntakeSubsystem extends SubsystemBase {
 
 	// ==================== State Query Methods ====================
 
+  public Angle minPivotAngle = Degrees.of(0);
+  public Angle maxPivotAngle = Degrees.of(360);
+
 	public boolean isPivotAtPosition() {
 		boolean atPositionRaw = Math.abs(pivotEncoder.getPosition() - targetPivotAngle.in(Degrees)) < PIVOT_ANGLE_TOLERANCE
 				.in(Degrees);
@@ -149,6 +152,19 @@ public class IntakeSubsystem extends SubsystemBase {
 	}
 
 	// ==================== Command Factory Methods ====================
+
+  public Command homeCommand() {
+    return
+      runOnce(() -> setPivotAngle(minPivotAngle))
+      .andThen(idle().until(this::isPivotStalled))
+      .andThen(runOnce(() -> setPivotAngle(Degrees.of(pivotEncoder.getPosition()))))
+      .andThen(runOnce(() -> minPivotAngle = Degrees.of(pivotEncoder.getPosition())))
+      .andThen(() -> setPivotAngle(maxPivotAngle))
+      .andThen(idle().until(this::isPivotStalled))
+      .andThen(runOnce(() -> setPivotAngle(Degrees.of(pivotEncoder.getPosition()))))
+      .andThen(runOnce(() -> maxPivotAngle = Degrees.of(pivotEncoder.getPosition())))
+      .withName("Home Intake");
+  }
 
 	public Command extendCommand() {
 		return runOnce(() -> setPivotAngle(PIVOT_EXTENDED_ANGLE))
