c62544b2af2800cddcaa3ce7ba94792d29e7ce9f
df <df1317@proton.me>
2026-03-10 20:45:38 -0400

    feat: set up on the fly PIDF hopper tuning

--- src/main/java/frc/robot/subsystems/hopper/HopperSubsystem.java
+++ src/main/java/frc/robot/subsystems/hopper/HopperSubsystem.java
@@ -1,7 +1,19 @@
 package frc.robot.subsystems.hopper;
 
 import static edu.wpi.first.units.Units.RPM;
-import static frc.robot.Constants.HopperConstants.*;
+import static frc.robot.Constants.HopperConstants.FEED_SPEED;
+import static frc.robot.Constants.HopperConstants.GEAR_RATIO;
+import static frc.robot.Constants.HopperConstants.HOPPER_CURRENT_LIMIT;
+import static frc.robot.Constants.HopperConstants.HOPPER_I_ZONE;
+import static frc.robot.Constants.HopperConstants.HOPPER_KD;
+import static frc.robot.Constants.HopperConstants.HOPPER_KI;
+import static frc.robot.Constants.HopperConstants.HOPPER_KP;
+import static frc.robot.Constants.HopperConstants.HOPPER_KV;
+import static frc.robot.Constants.HopperConstants.HOPPER_MOTOR_ID;
+import static frc.robot.Constants.HopperConstants.HOPPER_VELOCITY_TOLERANCE;
+import static frc.robot.Constants.HopperConstants.INVERTED;
+import static frc.robot.Constants.HopperConstants.REVERSE_SPEED;
+
 import com.revrobotics.PersistMode;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.ResetMode;
@@ -11,12 +23,14 @@ import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.config.SparkMaxConfig;
+
 import dev.doglog.DogLog;
 import edu.wpi.first.networktables.DoubleSubscriber;
 import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.HopperConstants;
 
 /**
  * Hopper subsystem
@@ -37,7 +51,7 @@ public class HopperSubsystem extends SubsystemBase {
 	private final HopperTelemetry telemetry;
 
 	// ==================== Test Mode ====================
-	private final DoubleSubscriber testHopperRPM = DogLog.tunable("Test/HopperRPM", 0.5);
+	private final DoubleSubscriber testHopperRPM = DogLog.tunable("Test/HopperRPM", 2000.0);
 
 	public HopperSubsystem() {
 		hopperMotor = new SparkMax(HOPPER_MOTOR_ID, MotorType.kBrushless);
@@ -53,15 +67,46 @@ public class HopperSubsystem extends SubsystemBase {
 		SparkMaxConfig config = new SparkMaxConfig();
 		config.idleMode(IdleMode.kCoast).smartCurrentLimit(HOPPER_CURRENT_LIMIT)
 				.inverted(INVERTED);
+		config.encoder
+				// Converts encoder rotations to degrees: (360 deg/rot) / gear_ratio
+				.positionConversionFactor(360.0 / GEAR_RATIO);
 		config.closedLoop.pid(HOPPER_KP, HOPPER_KI, HOPPER_KD).iZone(HOPPER_I_ZONE);
 		config.closedLoop.feedForward.kV(HOPPER_KV);
 
 		hopperMotor.configure(config, ResetMode.kResetSafeParameters, PersistMode.kNoPersistParameters);
 	}
 
+	private final DoubleSubscriber KP = DogLog.tunable("Test/KP", HopperConstants.HOPPER_KP);
+	private final DoubleSubscriber KI = DogLog.tunable("Test/KI", HopperConstants.HOPPER_KI);
+	private final DoubleSubscriber KD = DogLog.tunable("Test/KP", HopperConstants.HOPPER_KD);
+	private final DoubleSubscriber KV = DogLog.tunable("Test/KV", HopperConstants.HOPPER_KV);
+	private final DoubleSubscriber KS = DogLog.tunable("Test/KS", HopperConstants.HOPPER_KS);
+
+	double prevKP = KP.getAsDouble();
+	double prevKI = KI.getAsDouble();
+	double prevKD = KD.getAsDouble();
+	double prevKV = KV.getAsDouble();
+	double prevKS = KS.getAsDouble();
+
 	@Override
 	public void periodic() {
 		telemetry.log();
+		if (prevKP != KP.getAsDouble() || prevKI != KI.getAsDouble() || prevKD != KD.getAsDouble()
+				|| prevKV != KV.getAsDouble()) {
+
+			prevKP = KP.getAsDouble();
+			prevKI = KI.getAsDouble();
+			prevKD = KD.getAsDouble();
+			prevKV = KV.getAsDouble();
+
+			SparkMaxConfig config = new SparkMaxConfig();
+			config.idleMode(IdleMode.kCoast).smartCurrentLimit(HOPPER_CURRENT_LIMIT)
+					.inverted(INVERTED);
+			config.closedLoop.pid(KP.getAsDouble(), KI.getAsDouble(), KD.getAsDouble()).iZone(HOPPER_I_ZONE);
+			config.closedLoop.feedForward.kV(KV.getAsDouble());
+
+			hopperMotor.configure(config, ResetMode.kResetSafeParameters, PersistMode.kNoPersistParameters);
+		}
 	}
 
 	// ==================== State Query Methods ====================
