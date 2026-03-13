03bf88912a0ed2057e74225ec87b1cb66adf4db3
Kieran Klukas <me@dunkirk.sh>
2026-03-12 21:11:26 -0400

    feat: update the cameras positions

--- src/main/java/frc/robot/subsystems/swervedrive/Vision.java
+++ src/main/java/frc/robot/subsystems/swervedrive/Vision.java
@@ -36,8 +36,8 @@ import static edu.wpi.first.units.Units.Microseconds;
 import static edu.wpi.first.units.Units.Seconds;
 
 /**
- * PhotonVision-based vision system for AprilTag pose estimation.
- * Multi-camera support with outlier rejection and dynamic std devs.
+ * PhotonVision-based vision system for AprilTag pose estimation. Multi-camera support with outlier rejection and
+ * dynamic std devs.
  */
 public class Vision {
 
@@ -138,10 +138,10 @@ public class Vision {
 	}
 
 	public enum Cameras {
-		CENTER_CAM(
+		LEFT_CAM(
 				"PEBBLE",
-				new Rotation3d(0, Units.degreesToRadians(15.0), 0),
-				new Translation3d(0.32, 0.32, 0.30),
+				new Rotation3d(0, Units.degreesToRadians(20.0), 0),
+				new Translation3d(Units.inchesToMeters(13.5), Units.inchesToMeters(8.5), Units.inchesToMeters(10.5)),
 				VecBuilder.fill(
 						VisionConstants.CameraStdDevs.SINGLE_TAG[0],
 						VisionConstants.CameraStdDevs.SINGLE_TAG[1],
@@ -153,8 +153,21 @@ public class Vision {
 
 		BACK_CAM(
 				"STONE",
-				new Rotation3d(0, Units.degreesToRadians(15.0), Units.degreesToRadians(180)),
-				new Translation3d(-0.32, -0.32, 0.30),
+				new Rotation3d(0, 0.0, Units.degreesToRadians(90)),
+				new Translation3d(Units.inchesToMeters(0), Units.inchesToMeters(0), Units.inchesToMeters(15)),
+				VecBuilder.fill(
+						VisionConstants.CameraStdDevs.SINGLE_TAG[0],
+						VisionConstants.CameraStdDevs.SINGLE_TAG[1],
+						VisionConstants.CameraStdDevs.SINGLE_TAG[2]),
+				VecBuilder.fill(
+						VisionConstants.CameraStdDevs.MULTI_TAG[0],
+						VisionConstants.CameraStdDevs.MULTI_TAG[1],
+						VisionConstants.CameraStdDevs.MULTI_TAG[2])),
+
+		CENTER_CAM(
+				"BOULDER",
+				new Rotation3d(0, Units.degreesToRadians(20.0), Units.degreesToRadians(180)),
+				new Translation3d(Units.inchesToMeters(-13.5), Units.inchesToMeters(8.5), Units.inchesToMeters(10.5)),
 				VecBuilder.fill(
 						VisionConstants.CameraStdDevs.SINGLE_TAG[0],
 						VisionConstants.CameraStdDevs.SINGLE_TAG[1],
@@ -164,9 +177,9 @@ public class Vision {
 						VisionConstants.CameraStdDevs.MULTI_TAG[1],
 						VisionConstants.CameraStdDevs.MULTI_TAG[2]));
 
-		private final String cameraName;
 		public final PhotonCamera camera;
 		public final PhotonPoseEstimator poseEstimator;
+		private final String cameraName;
 		private final Matrix<N3, N1> singleTagStdDevs;
 		private final Matrix<N3, N1> multiTagStdDevs;
 		private final Transform3d robotToCamTransform;
