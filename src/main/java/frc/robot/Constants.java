f5e9953a806c46b200fa6cdaf75cfb3df60566b8
Kieran Klukas <me@dunkirk.sh>
2026-03-13 21:46:37 -0400

    chore: update pid for hood

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -126,7 +126,7 @@ public final class Constants {
 
 		public static final double FEEDER_RPM = 3000;
 
-		public static final double HOOD_KP = 0.0012;
+		public static final double HOOD_KP = 0.013;
 		public static final double HOOD_KI = 0.0;
 		public static final double HOOD_KD = 0.000;
 
